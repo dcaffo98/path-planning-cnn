4b0860f4835414dee4396bc83cf4c93d27b99e2e
Davide Caffagni <davidecaffagni98@gmail.com>
2022-04-27 13:27:30 +0200

    add filename in getitem

--- dataset/map_dataset.py
+++ dataset/map_dataset.py
@@ -23,18 +23,23 @@ class MapDataset(Dataset):
 
     def __getitem__(self, idx):
         if self._lazy:
-            return MapSample.load(self.samples[idx])
+            return MapSample.load(self.samples[idx]), self.samples[idx]
         else:
-            return self.samples[idx]
+            return self.samples[idx], self.samples[idx]
 
 
 if __name__ == '__main__':
     import cv2
-    abs_path = os.path.abspath('map_dataset')
+    abs_path = os.path.abspath('map_dataset_tmp')
     for file in os.listdir(abs_path):
         if os.path.isfile(os.path.join(abs_path, file)):
             sample = MapSample.load(os.path.join(abs_path, file))
             map = cv2.resize(sample.bgr_map(), (600, 600))
             cv2.imshow('sample', map)
+            import numpy as np
+            import torch
+            sample.path = torch.tensor([])
+            map = cv2.resize(sample.bgr_map(), (600, 600))
+            cv2.imshow('sample2', map)
             cv2.waitKey(0)
     
